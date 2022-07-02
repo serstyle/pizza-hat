d757e5a13a002d602f709780b692089aae56f520
Francois Desobry <desobry.f@gmail.com>
2022-07-02 12:36:01 +0200

    use Button component in RestaurantCard

--- components/Restaurant/RestaurantCard.tsx
+++ components/Restaurant/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { ISortedRestaurant } from '../../types';
+import { Button } from '../Button/Button';
 
 import s from './RestaurantCard.module.css';
 
@@ -16,9 +17,7 @@ export const RestaurantCard = ({ restaurant }: IProps) => {
                 <p>{restaurant.address1}</p>
                 <p className="text-gray-400">{restaurant.address2}</p>
                 <p className="text-gray-400">{restaurant.distanceFromUser.toFixed(2)}KM away from you</p>
-                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded mt-4">
-                    See Menu
-                </button>
+                <Button text={'See Menu'}/>
             </a>
         </Link>
     );
