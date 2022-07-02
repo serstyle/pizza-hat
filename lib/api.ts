1ce0bc07cefec5ca275f030236806c5edcff1d28
Francois Desobry <desobry.f@gmail.com>
2022-07-02 17:44:45 +0200

    add postOrder api and getOrder api

--- lib/api.ts
+++ lib/api.ts
@@ -1,25 +1,81 @@
-import { IMenuItem, IRestaurant } from '../types';
+import { ICartItem, IMenuItem, IOrderResponse, IOrderResponseWithCart, IOrderResponseWithFullCart, IRestaurant } from '../types';
+import { pizzaAppEndpoint } from './const/pizzaapp';
 
 export async function loadRestaurants(): Promise<Array<IRestaurant>> {
-    const res = await fetch('https://private-anon-d884c0cb85-pizzaapp.apiary-mock.com/restaurants/');
+    const res = await fetch(`${pizzaAppEndpoint}/restaurants/`);
     const data = await res.json();
 
     return data;
 }
-
 
 // This API always return the same restaurant no matter the param provided
-export async function getRestaurant(id:string): Promise<IRestaurant> {
-    const res = await fetch(`https://private-anon-d884c0cb85-pizzaapp.apiary-mock.com/restaurants/${id}`);
+// export async function getRestaurant(id: string): Promise<IRestaurant> {
+//     const res = await fetch(`${pizzaAppEndpoint}/restaurants/${id}`);
+//     const data = await res.json();
+//     return data;
+// }
+export async function getRestaurant(id: string): Promise<IRestaurant | null> {
+    const restaurants = await loadRestaurants();
+    const restaurant = restaurants.find((r) => r.id === +id);
+    if(restaurant) return restaurant;
+    else return null;
+}
+
+export async function getMenu(id: string): Promise<Array<IMenuItem>> {
+    const res = await fetch(`${pizzaAppEndpoint}/restaurants/${id}/menu`);
+    const data = await res.json();
+
+    return data;
+}
+export async function getOrder(orderId: string): Promise<IOrderResponseWithCart> {
+    const res = await fetch(`${pizzaAppEndpoint}/orders/${orderId}`);
     const data = await res.json();
     return data;
 }
 
-export async function getMenu(id:string): Promise<Array<IMenuItem>> {
-    const res = await fetch(`https://private-anon-d884c0cb85-pizzaapp.apiary-mock.com/restaurants/${id}/menu`);
-    const data = await res.json();
-
+export async function getOrderWithMenuAndRestaurant(orderId: string): Promise<IOrderResponseWithFullCart> {
+    const order = await getOrder(orderId);
+    //Why this restUArantId missspelling from the body response
+    const restaurant = await getRestaurant(`${order.restuarantId}`);
+    const menu = await getMenu(`${order.restuarantId}`);
+    const data = {
+        ...order,
+        orderedAt: new Date(order.orderedAt).toLocaleString(),
+        esitmatedDelivery: new Date(order.esitmatedDelivery).toLocaleString(),
+        restaurant,
+        cart: order.cart.map((cartItem) => {
+            const orderCart = menu.find((menuItem) => menuItem.id === cartItem.menuItemId) || null;
+            
+            return orderCart && {
+                ...orderCart,
+                quantity: cartItem.quantity
+            }
+        })
+    }
     return data;
 }
 
 
+export async function postOrder(cart: ICartItem[], restaurantId: string): Promise<IOrderResponse | null> {
+    const newCart = cart.map((cartItem) => {
+        return {
+            menuItemId: cartItem.id,
+            quantity: cartItem.quantity 
+        }
+    })
+
+    const res = await fetch(`${pizzaAppEndpoint}/orders/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({cart: newCart, restaurantId}) 
+    });
+    try {
+        const data = await res.json();
+        return data;
+
+    } catch (e) {
+        return null;
+    }
+}
--- types/index.ts
+++ types/index.ts
@@ -18,3 +18,28 @@ export interface IMenuItem {
     price: number;
     rank?: number;
 }
+
+export interface ICartItem extends IMenuItem {
+    quantity: number;
+}
+
+export interface IOrderResponse {
+    orderId: number;
+    totalPrice: number;
+    orderedAt: string;
+    esitmatedDelivery: string;
+    status: string;
+}
+
+export interface IOrderResponseWithCart extends IOrderResponse {
+    cart: {
+        menuItemId: number;
+        quantity: number;
+    }[];
+    restuarantId: number;
+}
+export interface IOrderResponseWithFullCart extends IOrderResponse {
+    restaurant: IRestaurant | null;
+    cart: Array<ICartItem | null>;
+    restuarantId: number;
+}
